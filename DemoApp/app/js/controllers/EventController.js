74dee46f66d8456e42d9719c2475000ef947f665
Alla <alla.hoffman@buildium.com>
2014-09-10 15:58:08 -0400

    added display and info for sessions, beginning to implement voting

--- DemoApp/app/js/controllers/EventController.js
+++ DemoApp/app/js/controllers/EventController.js
@@ -11,16 +11,31 @@ eventsApp.controller('EventController',
                 city: 'Mountain View',
                 province: "CA"
             },
-            imageUrl: '/img/such_fancy_doge.jpg',
+            imageUrl: '/img/dragon.jpg',
             sessions: [
                 {
-                    name: 'Directives Masterclass'
+                    name: 'Directives Masterclass',
+                    creatorName: 'Joe Schmoe',
+                    duration: '11 hours',
+                    level: 'Advanced',
+                    abstract: "I will talk so much about directives, you don't know.",
+                    upVoteCount: 0
                 },
                 {
-                    name: 'Do The TimeWarp Again'
+                    name: 'Do The TimeWarp Again',
+                    creatorName: 'Dr. Frankenfurter',
+                    duration: '1 hours',
+                    level: 'Advanced',
+                    abstract: "LET'S DO THE TIME WARP AGAIN.",
+                    upVoteCount: 0
                 },
                 {
-                    name: 'Bees!'
+                    name: 'Bees!',
+                    creatorName: 'Sherlock Holmes',
+                    duration: '2 hours',
+                    level: 'Intermediate',
+                    abstract: "BzzzzZZZzzzzzdeduction.",
+                    upVoteCount: 0
                 }
             ]
         }
