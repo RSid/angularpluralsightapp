11fa09333e6d59cecad75462791e4311127e50d4
Alla <alla.hoffman@buildium.com>
2014-09-10 15:39:55 -0400

    displaying event, image, and sessions added to data but not display. also added bootstrap styling.

---
+++ DemoApp/app/js/controllers/EventController.js
@@ -0,0 +1,28 @@
+﻿'user strict';
+
+eventsApp.controller('EventController',
+    function EventController($scope) {
+        $scope.event = {
+            name: 'Angular Bootcamp',
+            date: '1/1/2008',
+            time: '10:30 am',
+            location: {
+                address: "Google's HQ",
+                city: 'Mountain View',
+                province: "CA"
+            },
+            imageUrl: '/img/such_fancy_doge.jpg',
+            sessions: [
+                {
+                    name: 'Directives Masterclass'
+                },
+                {
+                    name: 'Do The TimeWarp Again'
+                },
+                {
+                    name: 'Bees!'
+                }
+            ]
+        }
+    }
+);
